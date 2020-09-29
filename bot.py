ec4e553a19e86075ff3d870945d6b4e02ed616ba
Oleg <r0tt3n-m3m0ry@protonmail.com>
2020-09-30 01:09:38 +0300

    Update

--- bot.py
+++ bot.py
@@ -3,58 +3,75 @@ try:
 except ModuleNotFoundError:
     print('VK API pip module not found. Please install it before running this script.'); exit()
 
-import pickle
+from datetime import datetime
+import time
 
-def get_vk_name(vk, user_id): # return vk first and last names of owner user_id
-    first_name = vk.users.get(user_ids=user_id)['first_name']
-    last_name = vk.users.get(user_ids=user_id)['last_name']
-    return list(first_name, last_name)
+def send_message(user_id, message):
+    if message[0] == '@':
+        vk.messages.send(user_id=user_id, random_id=vk_api.utils.get_random_id(), message='Держи)', attachment=message[1:])
+    else:
+        vk.messages.send(user_id=user_id, random_id=vk_api.utils.get_random_id(), message=message)
 
-def find_new_users(vk, list_of_groups): # create pickle dump with users ids - groups members
-    users = {}
+messages_for_mailing = ['Привет, я тебе скину трек, если он тебе понравится, сможешь проявить активность в группе в качестве подписки или лайка ? А если тебе очень понравится, сможешь оформить подписочку, чтобы не пропустить самое интересное ? Примерно через 2 недели выйдет новый трек ☺ ']
+users_for_mailing = []
 
-    for group_id in list_of_groups:
-        for offset in range(0, vk.groups.getMembers(group_id=group_id)['count'], 1000):
-            for user_id in vk.groups.getMembers(group_id=gruoup_id, count=1000, offset=offset)['items']:
-                try:
-                    print(vk.users.get(user_ids=user_id))[0]['deactivated']
-                except KeyError:
-                    if vk.users.get(user_ids=user_id, fields='can_write_private_message')['can_write_private_message'] == 1:
-                        users[user_id] = 0
+vk_login = input('VK login: ')
+vk_password = input('VK password: ')
 
-    with open('users.txt', 'wb') as file_users:
-        pickle.dump(users, file_users)
+vk_app_id = '2685278'
 
-def send_message(vk, user_id, message):
-    vk.messages.send(user_id=user_id, random_id=vk_api.utils.get_random_id(), message=message)
+while True:
+    try:
+        vk_session = vk_api.VkApi(login=vk_login, password=vk_password, app_id=vk_app_id)
+        vk_session.auth()
+        vk = vk_session.get_api()
+    except vk_api.exceptions.BadPassword:
+        print('Bad password!')
+    except vk_api.exceptions.AuthError:
+        print('Authorization error! Account deactivated!')
+    else:
+        print('Successfully logged in!')
+        break
 
-messages_for_spaming = ['1', '2', '3']
+if len(users_for_mailing) == 0:
+    print('Enter user_id\'s for mail him or list with user_id\'s. User must be opened for income messages from you account. If not, this will be reported in the console and his account will be deleted from mailing list.\n\nIf you enter all user_id\'s, enter just 0.')
 
-try:
-    with open('users.txt', 'rb') as file_users:
-        users_for_spamming = pickle.load(file_users)
-except FileNotFoundError:
-    print('First run this script with \'-i\' flag and call find_new_users for filling file with user_ids'); exit()
+    while True:
+        user_id = input(f'USER_ID ({len(users_for_mailing)+1}) : ')
 
-accounts = {}
-
-with open('accounts.txt') as file_accounts:
-    for account_credentials in file_accounts:
-        vk_login = account_credentials.split(':')[0]
-        vk_password = account_credentials.split(':')[1]
-
-        try:
-            vk_session = vk_api.VkApi(login=vk_login, password=vk_password, app_id='2685278') # app_id from kate mobile for vk_api messages bypass
-            vk_session.auth()
-        except vk_api.exceptions.BadPassword:
-            print(f'Account {vk_login}:{vk_password} - Bad Password. Ignored.')
-            continue
-        except vk_api.exceptions.AuthError:
-            print(f'Account {vk_login}:{vk_password} - Authorization Error. Ignored.')
-            continue
+        if user_id[0] == '[' and user_id[-1] == ']':
+            for user_id_from_list in user_id[1:-1].split(','):
+                users_for_mailing.append(user_id_from_list.strip())
         else:
-            print(f'Account {vk_login}:{vk_password} - Successful logged in!')
-            vk = vk_session.get_api()
-            accounts[vk] = 0
+            if user_id != '0':
+                users_for_mailing.append(user_id)
+            else:
+                break
 
-print(f'Active accounts: {len(accounts)}')
+if len(messages_for_mailing) == 0:
+    print('Enter messagess for mail them or list with messages. For send attachment, start message from symbor \'@\', and after write attachment in VK attachments format. If you enter all messagess, enter just 0.')
+
+    while True:
+        message = input(f'MESSAGE ({len(messages_for_mailing)+1}) : ')
+
+        if message[0] == '[' and message[-1] == ']':
+            for message_from_list in message[1:-1].split(','):
+                messages_for_mailing.append(message_from_list.strip())
+        else:
+            if message != '0':
+                messages_for_mailing.append(message)
+            else:
+                break
+
+for message in messages_for_mailing:
+    for user_id in users_for_mailing:
+        try:
+            send_message(user_id, message)
+            print(f'[{datetime.now().strftime("%H:%M:%S")}] Message sent!')
+        except:
+            print(f'[{datetime.now().strftime("%H:%M:%S")}] Message cannot be sent!')
+        time.sleep(10)
+    print(f'[{datetime.now().strftime("%H:%M:%S")}] Wait 5 minutes...')
+    time.sleep(120)
+
+print('Mailing completed!')
